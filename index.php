df2ef8ab56812e67b183c6257ea144a4f51d9505
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-09 19:52:14 +0200

    create form & pass generator function

--- index.php
+++ index.php
@@ -8,7 +8,29 @@
 </head>
 <body>
   
-  
+  <form action="index.php" method="POST" oninput="result.value=parseInt(password-length.value)">
+    <!-- <input type="text" name="password-length"> -->
+    <input type="number" name="password-length" min="8" max="16" placeholder="From 8 to 16">
+    <!-- <input type="range" name="password-length" min="8" max="16"> -->
+    <input type="submit">
+    <input type="reset">
+
+    <?php
+      $lengthChosen = $_POST['password-length'];
+
+      function generatePassword($lengthChosen) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"£$%&/()=?^*ç°§,.-;:_+';
+        $charactersLength = strlen($characters);
+        $result = '';
+        for($i = 0; $i < $lengthChosen; $i++) {
+          $result .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $result;
+      }
+    ?>
+
+    <div><?php echo $lengthChosen ?></div>
+  </form>
 
 </body>
 </html>
